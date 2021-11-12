a15085fb867b7725272cdd9a1004630eb58e28e2
Nik020304 <94171709+Nik020304@users.noreply.github.com>
2021-11-12 16:02:53 +0530

    Add files via upload

---
+++ ASCII/Print ASCII value of given character.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+  char ch='\0';
+  
+  printf("enter a character");
+  ch=getchar();
+ 
+ printf("\n\t ASCII value of %c is %d.",ch,ch);
+  return 0;
+}
